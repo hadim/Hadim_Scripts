dd10d51be9e071edfbd1bb496adf55ecac7aac98
Hadrien Mary <hadrien.mary@gmail.com>
2015-01-26 13:25:39 +0100

    Small fix in crop script

--- plugins/Scripts/Plugins/Crop_Multi_Roi.py
+++ plugins/Scripts/Plugins/Crop_Multi_Roi.py
@@ -16,7 +16,7 @@ def main():
 
     # Get active dataset
     active_dataset = imgdisplay.getActiveDataset()
-    
+
     if not active_dataset:
         IJ.showMessage('No image opened.')
         return
@@ -26,10 +26,10 @@ def main():
 
     if not fname:
         IJ.showMessage('Source image needs to match a file on the system.')
-        return 
-    
+        return
+
     IJ.log('Image filename is %s' % fname)
-    
+
     # Iterate over all ROIs from ROI Manager
     rois = RoiManager.getInstance()
 
@@ -39,17 +39,17 @@ def main():
 
     rois_array = rois.getRoisAsArray()
     for i, roi in enumerate(rois_array):
-    
+
         crop_id = i +1
-        IJ.log("Opening crop %i / %i" % (crop_id, len(rois_array)))
-    
+        IJ.log("Croping %i / %i" % (crop_id, len(rois_array)))
+
         # Get ROI bounds
         bounds = roi.getBounds()
         x = bounds.x
         y = bounds.y
         w = bounds.width
-        h = bounds.height  
-    
+        h = bounds.height
+
         # Initiate dimensions
         dims = {}
         dim_names = [None] * 5
@@ -58,22 +58,21 @@ def main():
             dim_names[ax_index] = str(ax)
             if int(active_dataset.dimension(ax_index)) == 1:
                 dims[str(ax)] = (0, 0)
-            else: 
+            else:
                 dims[str(ax)] = (0, int(active_dataset.dimension(ax_index)) - 1)
-                
+
         # Set cropped regions
         dims['X'] = (x, x + w)
         dims['Y'] = (y, y + h)
-        
+
         # Set crop intervals
         begin_interval = [dims[name][0] for name in dim_names]
         end_interval = [dims[name][1] for name in dim_names]
         interval = FinalInterval(begin_interval, end_interval)
-        
+
         # Get filename and basename of the current cropped image
         crop_basename = "crop%i_%s" % (crop_id, active_dataset.getName())
         crop_fname = os.path.join(os.path.dirname(fname), crop_basename)
-        
 
         # Create dataset
         imp = ij.op().crop(interval, active_dataset.getImgPlus())
@@ -82,11 +81,11 @@ def main():
 
         # Show cropped image
         ij.ui().show(ds.getName(), ds)
-        
+
         # Save cropped image
         IJ.log("Saving crop to %s" % crop_fname)
         data.save(ds, crop_fname)
-    
+
     IJ.log('Done')
 
 main()
