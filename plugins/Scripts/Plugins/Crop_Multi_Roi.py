52ba70ab58ca0e4b0a472ada4474400a6bda17c4
Hadrien Mary <hadrien.mary@gmail.com>
2015-01-27 12:16:47 +0100

    Factorize

--- plugins/Scripts/Plugins/Crop_Multi_Roi.py
+++ plugins/Scripts/Plugins/Crop_Multi_Roi.py
@@ -1,5 +1,5 @@
-# @DatasetService data
-# @ImageDisplayService imgdisplay
+# @DatasetService datasetservice
+# @ImageDisplayService displayservice
 # @ImageJ ij
 # @AbstractLogService log
 # @DefaultLegacyService legacy
@@ -7,19 +7,21 @@
 from ij import IJ
 from ij.plugin.frame import RoiManager
 
-from net.imagej.axis import Axes
 from net.imagej import DefaultDataset
-from net.imglib2 import FinalInterval
 from io.scif.img import ImgSaver
 
 import os
+import sys
+
+sys.path.append(os.path.join(IJ.getDirectory('plugins'), "Scripts", "Plugins"))
+from libtools import crop
 
 def main():
 
     # Get active dataset
     img = IJ.getImage()
     display = legacy.getInstance().getImageMap().lookupDisplay(img)
-    active_dataset = imgdisplay.getActiveDataset(display)
+    active_dataset = displayservice.getActiveDataset(display)
 
     if not active_dataset:
         IJ.showMessage('No image opened.')
@@ -47,55 +49,26 @@ def main():
         crop_id = i +1
         IJ.log("Croping %i / %i" % (crop_id, len(rois_array)))
 
-        # Get ROI bounds
-        bounds = roi.getBounds()
-        x = bounds.x
-        y = bounds.y
-        w = bounds.width
-        h = bounds.height
-
-        # Initiate dimensions
-        dims = {}
-        dim_names = [None] * 5
-        for ax in [Axes.X, Axes.Y, Axes.Z, Axes.TIME, Axes.CHANNEL]:
-            ax_index = active_dataset.dimensionIndex(ax)
-            dim_names[ax_index] = str(ax)
-            if int(active_dataset.dimension(ax_index)) == 1:
-                dims[str(ax)] = (0, 0)
-            else:
-                dims[str(ax)] = (0, int(active_dataset.dimension(ax_index)) - 1)
-
-        # Set cropped regions
-        dims['X'] = (x, x + w)
-        dims['Y'] = (y, y + h)
-
-        # Set crop intervals
-        begin_interval = [dims[name][0] for name in dim_names]
-        end_interval = [dims[name][1] for name in dim_names]
-        interval = FinalInterval(begin_interval, end_interval)
-
         # Get filename and basename of the current cropped image
         crop_basename = "crop%i_%s" % (crop_id, active_dataset.getName())
         crop_fname = os.path.join(os.path.dirname(fname), crop_basename)
 
-        # Create dataset
-        imp = ij.op().crop(interval, active_dataset.getImgPlus())
-        ds = data.create(imp)
-        ds.setName(crop_basename)
+        # Get bounds and crop
+        bounds = roi.getBounds()
+        dataset = crop(ij, datasetservice, active_dataset,
+                       bounds.x, bounds.y, bounds.width,
+                       bounds.height, crop_basename)
 
         # Show cropped image
-        ij.ui().show(ds.getName(), ds)
+        ij.ui().show(dataset.getName(), dataset)
 
-        # Save cropped image
+        # Save cropped image (ugly hack)
         IJ.log("Saving crop to %s" % crop_fname)
-        #data.save(ds, crop_fname)
-        
-        # Hack to get save works
         imp = IJ.getImage()
         IJ.run("Properties...", "frame=5")
         IJ.run("Bio-Formats Exporter", "save=" + crop_fname + " compression=Uncompressed")
         imp.close()
-        
+
     IJ.log('Done')
 
 main()
--- various/utils.py
+++ various/utils.py
@@ -1,7 +1,4 @@
 # @DatasetService data
-# @ImageDisplayService imgdisplay
-# @ImageJ ij
-# @DefaultLegacyService legacy
 
 from ij import IJ
 import os
@@ -10,7 +7,6 @@ path = "/media/thor/data/microscopy_data/ndc80-gfp-cdc11-cfp-beginning-of-mitosi
 
 def process(root, f):
     print(f)
-    root, f = ('/media/thor/data/microscopy_data/ndc80-gfp-cdc11-cfp-beginning-of-mitosis/movies/wt/2015.01.23/movie_9', 'crop3_movie_9_MMStack_Pos0.ome.tif')
     full_path = os.path.join(root, f)
     dest_path = os.path.join(root, "tmp", f)
     if not os.path.isdir(os.path.join(root, "tmp")):
