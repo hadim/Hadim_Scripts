565a893b3feb2ace137c8cd610658f58dc731e94
Hadrien Mary <hadrien.mary@gmail.com>
2016-11-10 00:37:29 -0500

    Improve In_Vivo_Nucleation_Rate.py

--- plugins/Scripts/Plugins/In_Vivo_Nucleation_Rate.py
+++ plugins/Scripts/Plugins/In_Vivo_Nucleation_Rate.py
@@ -1,13 +1,13 @@
-# @Integer(label="Centrosome 1 X Position", required=true) cen1_x
-# @Integer(label="Centrosome 1 Y Position", required=true) cen1_y
-# @Integer(label="Centrosome 2 X Position", required=true) cen2_x
-# @Integer(label="Centrosome 2 Y Position", required=true) cen2_y
 # @Float(label="dt (sec)", required=true, value=3) dt
-# @Integer(label="Radius", required=true, value=30) radius
-# @Integer(label="Line Width for Kymo", required=true, value=4) line_width
+# @Integer(label="Number of points for Kymo Line", required=true, value=20) n_points_circle
+# @Integer(label="Line Width for Kymo Line (pixel)", required=true, value=4) line_width
+# @Integer(label="Radius of the line to make the kymograph (pixel)", required=true, value=25) radius
+# @Float(label="Radius (for comet detection on kymograph) (pixel)", required=true, value=5) radius_detection
+# @Boolean(label="Close all opened window after processing ?", value=False) close_windows
+# @Boolean(label="Show Results Tables ?", value=True) show_results
 
 # @ImageJ ij
-# @ImagePlus img
+# @ImagePlus imp
 # @Dataset data
 # @StatusService status
 # @LogService log
@@ -17,7 +17,6 @@
 
 import os
 import math
-import csv
 
 from net.imagej.axis import Axes
 from net.imagej.ops import Ops
@@ -39,6 +38,7 @@ from ij.gui import Roi
 from ij.gui import PolygonRoi
 from ij.gui import OvalRoi
 from ij.measure import Calibration
+from ij.measure import ResultsTable
 from ij import IJ
 
 from java.awt import Polygon
@@ -48,10 +48,13 @@ from java.awt import Polygon
 def z_project(data):
 	z_dim = data.dimensionIndex(Axes.Z)
 	projected_dimensions = [data.dimension(d) for d in range(0, data.numDimensions()) if d != z_dim]
+	axis = [data.axis(d) for d in range(0, data.numDimensions()) if d != z_dim]
 	z_projected = ops.create().img(projected_dimensions)
 	max_op = ops.op(Ops.Stats.Max, data)
 	ops.transform().project(z_projected, data, max_op, z_dim)
-	return ij.dataset().create(z_projected)
+	z_projected = ij.dataset().create(z_projected)
+	z_projected.setAxes(axis)
+	return z_projected
 
 
 def get_circle_points(x_center, y_center, radius, n=20):
@@ -75,135 +78,180 @@ def get_roi_manager(new=False):
 		
 ## Main Code
 
-# Init path and create the Analysis folder
-fname = data.getSource()
-dir_path = os.path.dirname(fname)
+def main():
+	# Check for points ROI on the image
+	roi = imp.getRoi()
+	if not roi or not roi.getType() == Roi.POINT:
+		status.warn("Please selection one or more points with the multi points tools.")
+		return 
+	
+	points = roi.getContainedPoints()
+	centrosomes = [[p.x, p.y] for p in points]
+	log.info("%i centrosomes detected" % len(points))
+	
+	# Set X and Y scale to 1
+	imp.getCalibration().pixelWidth = 1
+	imp.getCalibration().pixelHeight = 1
+	
+	# Init path and create the Analysis folder
+	fname = data.getSource()
+	dir_path = os.path.dirname(fname)
+	
+	analysis_dir = os.path.join(dir_path, "Analysis")
+	if not os.path.exists(analysis_dir):
+		os.makedirs(analysis_dir)
+	
+	# Do Z Projection
+	z_projected = z_project(data)
+	ij.ui().show("Z_Projection.tif", z_projected)
+	z_fname = os.path.join(analysis_dir, "Z_Projection.tif")
+	io.save(z_projected, z_fname)
+	log.info("Saving Z Projection to %s" % (z_fname))
 
-analysis_dir = os.path.join(dir_path, "Analysis")
-if not os.path.exists(analysis_dir):
-	os.makedirs(analysis_dir)
-
-# Do Z Projection
-
-z_projected = z_project(data)
-ij.ui().show("Z_Projection.tif", z_projected)
-z_fname = os.path.join(analysis_dir, "Z_Projection.tif")
-io.save(z_projected, z_fname)
-log.info("Saving Z Projection to %s" % (z_fname))
-
-z_projected = data
-
-# Calculcate the radius to be between both centrosomes
-if cen2_x == 0 and cen2_y == 0:
-	centrosomes = [[cen1_x, cen1_y]]
-else:
-	centrosomes = [[cen1_x, cen1_y], [cen2_x, cen2_y]]
-	cen_center_x = (cen1_x + cen2_x) / 2
-	cen_center_y = (cen1_y + cen2_y) / 2
-	#radius = math.sqrt((cen1_x - cen_center_x)**2 + (cen1_y - cen_center_y)**2)
-
-
-results = []
-for i, (cen_x, cen_y) in enumerate(centrosomes[:]):
-
-	name = "Cen%i_Kymograph" % (i+1)
-
-	# For now we can't use IJ2 ROI so let's use the IJ1 ROI infrastructure
-	IJ.selectWindow("Z_Projection.tif")
-
-	# Draw line around the centrosomes
-	xpoints, ypoints = get_circle_points(cen_x, cen_y, radius=radius, n=50)
-	circle = PolygonRoi(xpoints, ypoints, Roi.POLYLINE)
-	circle.setStrokeWidth(line_width)
-	rm = get_roi_manager(new=True)
-	rm.addRoi(circle)
-	rm.runCommand("Show All")
-
-	# Save the ROI
-	rm.runCommand("Save", os.path.join(analysis_dir, name + "_ROI.zip"))
-
-	# Create kymograph
-	kfactory = KFactory(ij.context(), data, circle)
-	kfactory.build()
-	kymograph = kfactory.getKymograph()
-
-	kymo_fname = os.path.join(analysis_dir, name + ".tif")
-	kymograph_imp = ImageJFunctions.wrap(kymograph, name + ".tif")
-	ij.ui().show(name + ".tif", kymograph_imp)
-
-	# Do background substraction
-	IJ.run(kymograph_imp, "Subtract Background...", "rolling=" + str(50))
+	# Iterate over all centrosomes
+	results = []
+	for i, (cen_x, cen_y) in enumerate(centrosomes[:]):
+	
+		name = "Cen%i_Kymograph" % (i+1)
+	
+		# Draw line around the centrosomes
+		xpoints, ypoints = get_circle_points(cen_x, cen_y, radius=radius, n=n_points_circle)
+		circle = PolygonRoi(xpoints, ypoints, Roi.POLYLINE)
+		circle.setStrokeWidth(line_width)
+		rm = get_roi_manager(new=True)
+		rm.addRoi(circle)
+	
+		# Save the ROI
+		rm.runCommand("Save", os.path.join(analysis_dir, name + "_Line_ROI.zip"))
+		rm.runCommand("Reset")
+	
+		# Create kymograph
+		kfactory = KFactory(ij.context(), z_projected, circle)
+		kfactory.build()
+		kymograph = kfactory.getKymograph()
+	
+		kymo_fname = os.path.join(analysis_dir, name + ".tif")
+		kymograph_imp = ImageJFunctions.wrap(kymograph, name + ".tif")
+		ij.ui().show(name + ".tif", kymograph_imp)
+	
+		# Do background substraction
+		IJ.run(kymograph_imp, "Subtract Background...", "rolling=" + str(50))
+			
+		# Save the kymo
+		IJ.save(kymograph_imp, kymo_fname) 
+		log.info("Saving Kymograph for Centrosome #%i to %s" % (i+1, kymo_fname))
+	
+		# Track spots
+		kymograph_imp.getCalibration().pixelWidth = 1
+		kymograph_imp.getCalibration().pixelHeight = 1
 		
-	# Save the kymo
-	IJ.save(kymograph_imp, kymo_fname) 
-	log.info("Saving Kymograph for Centrosome #%i to %s" % (i+1, kymo_fname))
+		model = Model()
+		model.setLogger(Logger.IJ_LOGGER)
+		
+		settings = Settings()
+		settings.setFrom(kymograph_imp)
+		
+		settings.detectorFactory = DogDetectorFactory()
+		settings.detectorSettings = {
+			'DO_SUBPIXEL_LOCALIZATION' : True,
+			'RADIUS' : radius_detection,
+			'TARGET_CHANNEL' : 1,
+			'THRESHOLD' : 0.,
+			'DO_MEDIAN_FILTERING' : True,
+		}
+		
+		filter1 = FeatureFilter('QUALITY', 0, True)
+		#settings.addSpotFilter(filter1)
+		
+		trackmate = TrackMate(model, settings)
+		trackmate.checkInput()
+		trackmate.execDetection()
+		trackmate.execInitialSpotFiltering()
+		trackmate.computeSpotFeatures(True)
+		trackmate.execSpotFiltering(True)
+		
+		# Do auto threshold
+		# threshold = TMUtils.otsuThreshold( valuesMap.get( selectedFeature ) )
+		
+		rm = get_roi_manager(new=True)
+		
+		spots = []
+		for spot in model.getSpots().iterator(True):
+			spots.append(spot)
+		
+			x = spot.getFeatures()['POSITION_X']
+			y = spot.getFeatures()['POSITION_Y']
+			spot_radius = spot.getFeatures()['RADIUS']
+		
+			spot_roi = OvalRoi(x - spot_radius/2, y - spot_radius/2, spot_radius, spot_radius)
+			rm.addRoi(spot_roi)
+	
+		spots_fname = os.path.join(analysis_dir, name + "_Comets_ROI.zip")
+		rm.runCommand("Save", spots_fname)
+		log.info("Saving detected spots to %s" % (spots_fname))
+	
+		duration = kymograph_imp.width * dt
+		events_number = len(spots)
+		rate = (events_number / duration) * 60
+		results.append([os.path.basename(dir_path), i+1, rate, duration, events_number])
 
-	# Track spots
-	kymograph_imp.getCalibration().pixelWidth = 1
-	kymograph_imp.getCalibration().pixelHeight = 1
-	
-	model = Model()
-	model.setLogger(Logger.IJ_LOGGER)
-	
-	settings = Settings()
-	settings.setFrom(kymograph_imp)
-	
-	settings.detectorFactory = DogDetectorFactory()
-	settings.detectorSettings = {
-		'DO_SUBPIXEL_LOCALIZATION' : True,
-		'RADIUS' : 4.0,
-		'TARGET_CHANNEL' : 1,
-		'THRESHOLD' : 0.,
-		'DO_MEDIAN_FILTERING' : True,
-	}
-	
-	filter1 = FeatureFilter('QUALITY', 0, True)
-	settings.addSpotFilter(filter1)
-	
-	trackmate = TrackMate(model, settings)
-	trackmate.checkInput()
-	trackmate.execDetection()
-	trackmate.execInitialSpotFiltering()
-	trackmate.computeSpotFeatures(True)
-	trackmate.execSpotFiltering(True)
-	
-	# Do auto threshold
-	# threshold = TMUtils.otsuThreshold( valuesMap.get( selectedFeature ) )
-	
-	rm = get_roi_manager(new=True)
-	
-	spots = []
-	for spot in model.getSpots().iterator(False):
-		spots.append(spot)
-	
-		x = spot.getFeatures()['POSITION_X']
-		y = spot.getFeatures()['POSITION_Y']
-		spot_radius = spot.getFeatures()['RADIUS']
-	
-		spot_roi = OvalRoi(x, y, spot_radius, spot_radius)
-		rm.addRoi(spot_roi)
+		if close_windows:
+			kymograph_imp.close()
 
-	spots_fname = os.path.join(analysis_dir, name + "_Comets_ROI.zip")
-	rm.runCommand("Show All")
-	rm.runCommand("Save", spots_fname)
-	log.info("Saving detected spots to %s" % (spots_fname))
+	if close_windows:
+		IJ.selectWindow("Z_Projection.tif")
+		IJ.getImage().close()
+	rm.runCommand("Reset")
+	
+	# Write results in csv
+	table = ResultsTable() 
+	for row in results:
+		table.incrementCounter()
+		table.addValue('Filename', row[0])
+		table.addValue('Centrosome #', row[1])
+		table.addValue('Nucleation Rate (min-1)', row[2])
+		table.addValue('Time (s)', row[3])
+		table.addValue('Nucleation Events', row[4])
+	if show_results:
+		table.show('Results Nucleation Rate')
+	result_fname = os.path.join(analysis_dir, "Results.csv")
+	table.save(result_fname)
+	log.info("Saving final results to %s" % (result_fname))
 
-	duration = kymograph_imp.height * dt
-	events_number = len(spots)
-	rate = (events_number / duration) * 60
-	results.append([os.path.basename(dir_path), i+1, rate, duration, events_number])
+	# Write centrosomes position in csv
+	table = ResultsTable() 
+	for i, row in enumerate(centrosomes):
+		table.incrementCounter()
+		table.addValue('Centrosome #', i+1)
+		table.addValue('x', row[0])
+		table.addValue('y', row[1])
+	if show_results:
+		table.show('Centrosomes Positions')
+	cen_fname = os.path.join(analysis_dir, "Centrosomes_Position.csv")
+	table.save(cen_fname)
+	log.info("Saving centrosomes position to %s" % (cen_fname))
 
-rm.runCommand("Reset")
-
-# Write results in csv
-result_fname = os.path.join(analysis_dir, "Results.csv")
-columns = [['Filename', "Centrosome #", 'Nucleation Rate (min-1)', 'Time (s)', 'Nucleation Events']]
-
-f = open(result_fname, 'wb')
-w = csv.writer(f, delimiter=',')
-w.writerows(columns + results)
-f.close()
-
-log.info("Saving final results to %s" % (result_fname))
-
-
+	# Write parameters in csv
+	table = ResultsTable() 
+	table.incrementCounter()
+	table.addValue('Value', "dt")
+	table.addValue('Key', dt)
+	table.incrementCounter()
+	table.addValue('Value', "n_points_circle")
+	table.addValue('Key', n_points_circle)
+	table.incrementCounter()
+	table.addValue('Value', "line_width")
+	table.addValue('Key', line_width)
+	table.incrementCounter()
+	table.addValue('Value', "radius")
+	table.addValue('Key', radius)
+	table.incrementCounter()
+	table.addValue('Value', "radius_detection")
+	table.addValue('Key', radius_detection)
+	if show_results:
+		table.show('Parameters')
+	para_fname = os.path.join(analysis_dir, "Parameters.csv")
+	table.save(para_fname)
+	log.info("Saving parameters to %s" % (para_fname))
+	
+main()
