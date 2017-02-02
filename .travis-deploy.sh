c2012b8c09fdf6eb953f9523511968ae771af5ab
Hadrien Mary <hadrien.mary@gmail.com>
2017-02-02 15:45:58 -0500

    Update travis script

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -22,4 +22,5 @@ mvn clean install -Dimagej.app.directory=$IJ_PATH -Ddelete.other.versions=true
 
 # Deploy the package
 $IJ_LAUNCHER --update edit-update-site $UPDATE_SITE $URL "webdav:$USER:$WIKI_UPLOAD_PASS" .
+#$IJ_LAUNCHER --update upload --update-site $UPDATE_SITE --force-shadow $IJ_PATH/jars/hadim-scripts-1.1.0-SNAPSHOT.jar
 $IJ_LAUNCHER --update upload-complete-site --force --force-shadow $UPDATE_SITE
