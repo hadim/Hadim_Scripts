2de2827a4a6b926681975826a43218d3d27ce23f
Hadrien Mary <hadrien.mary@gmail.com>
2017-05-19 19:17:16 -0400

    Fix combine script

--- src/main/resources/script_templates/Hadim_Scripts/Combine_Single_Frame_To_Stack.py
+++ src/main/resources/script_templates/Hadim_Scripts/Combine_Single_Frame_To_Stack.py
@@ -9,7 +9,7 @@ from net.imglib2.img.array import ArrayImgFactory
 from net.imagej.axis import Axes
 from net.imagej import ImgPlus
 
-nFrames = stack1.getFrames()
+nFrames = stack1.getFrames() if stack1.getFrames() > 1 else stack1.getDepth()
 
 stack1 = Views.dropSingletonDimensions(stack1)
 
@@ -20,3 +20,5 @@ stack2 = Views.dropSingletonDimensions(stack2)
 stack = Views.stack(list([stack1, stack2]))
 stack = ImgPlus(ImgView.wrap(stack, ArrayImgFactory()), "", [Axes.X, Axes.Y, Axes.TIME, Axes.CHANNEL])
 stack = ij.dataset().create(stack)
+
+ij.ui().show(stack)
