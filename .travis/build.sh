3b04cec7960ee5b801902ba22f4d39961b96fd4c
Hadrien Mary <hadrien.mary@gmail.com>
2017-10-23 14:16:41 -0400

    Fix travis bash

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,7 +2,7 @@
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh
 
-if [ -v ${TRAVIS_TAG} ];
+if [ -n ${TRAVIS_TAG} ];
 then
 	echo "Upload the package to update site.";
 
