36031d374d889be773dbba6603d27ad7d215b625
Hadrien Mary <hadrien.mary@gmail.com>
2018-10-11 15:59:50 -0400

    Fix bug in kymo comet velocity script

--- src/main/resources/script_templates/Hadim_Scripts/Kymograph_Analysis/Kymograph_Comet_Velocity.py
+++ src/main/resources/script_templates/Hadim_Scripts/Kymograph_Analysis/Kymograph_Comet_Velocity.py
@@ -4,7 +4,7 @@
 # @Float(label="Minimal length of a run (um)", value=0) minimal_length
 # @Boolean(label="Do you want to save result file ?", required=False, value=False) save_results
 # @Boolean(label="Do you want to save ROI file ?", required=False, value=False) save_roi
-# @Boolean(label="Assume x is length and y is time ?", value=True) is_x_length
+# @Boolean(label="Assume x is time and y is length ?", value=True) is_x_time
 # @ImageJ ij
 # @ImagePlus img
 # @Dataset data
@@ -18,87 +18,92 @@ from ij.measure import ResultsTable
 from ij.gui import Roi
 
 def main():
-	# Get image processor and imgplus
-	imp = img.getProcessor()
-	imgp = data.getImgPlus()
-	fname = data.getSource()
-	name  = os.path.basename(fname)
-	
-	# Get ROIManager
-	rm = RoiManager.getInstance()
-	if not rm:
-		status.warn("Use ROI Manager tool (Analyze>Tools>ROI Manager...).")
-		return False
+    # Get image processor and imgplus
+    imp = img.getProcessor()
+    imgp = data.getImgPlus()
+    fname = data.getSource()
+    name  = os.path.basename(fname)
 
-	if len(rm.getRoisAsArray()) == 0:
-		status.warn("ROI Manager does not have any ROI.")
-		return False
+    # Get ROIManager
+    rm = RoiManager.getInstance()
+    if not rm:
+        status.warn("Use ROI Manager tool (Analyze>Tools>ROI Manager...).")
+        return False
 
-	rt = ResultsTable()
+    if len(rm.getRoisAsArray()) == 0:
+        status.warn("ROI Manager does not have any ROI.")
+        return False
 
-	for i, roi in enumerate(rm.getRoisAsArray()):
+    rt = ResultsTable()
 
-		if roi.getType() == Roi.LINE:
-			x1 = roi.x1
-			y1 = roi.y1
-			x2 = roi.x2
-			y2 = roi.y2
+    for i, roi in enumerate(rm.getRoisAsArray()):
 
-		elif roi.getType() == Roi.FREELINE or roi.getType() == Roi.POLYLINE:
-			xpoints = roi.getInterpolatedPolygon(1, True).xpoints
-			ypoints = roi.getInterpolatedPolygon(1, True).ypoints
+        if roi.getType() == Roi.LINE:
+            x1 = roi.x1
+            y1 = roi.y1
+            x2 = roi.x2
+            y2 = roi.y2
 
-			# Don't use this ROI if it has only one point
-			if len(xpoints) < 2 or len(xpoints) < 2:
-				continue
-			
-			x1 = xpoints[0]
-			y1 = ypoints[0]
-			x2 = xpoints[-1]
-			y2 = ypoints[-1]
+        elif roi.getType() == Roi.FREELINE or roi.getType() == Roi.POLYLINE:
+            xpoints = roi.getInterpolatedPolygon(1, True).xpoints
+            ypoints = roi.getInterpolatedPolygon(1, True).ypoints
 
-		else:
-			continue
+            # Don't use this ROI if it has only one point
+            if len(xpoints) < 2 or len(xpoints) < 2:
+                continue
 
-		if is_x_length:
-			x1, y1 = y1, x1
-			x2, y2 = y2, x2
+            x1 = xpoints[0]
+            y1 = ypoints[0]
+            x2 = xpoints[-1]
+            y2 = ypoints[-1]
 
-		if x1 > x2:
-			x1, x2 = x2, x1
-			y1, y2 = y2, y1
+        else:
+            continue
 
-		# Compute line features
-		run_length = (y1 - y2) * pixel_length
-		run_duration = (x2 - x1) * dt
-		run_speed = run_length / run_duration
+        if is_x_time:
+            x1, y1 = y1, x1
+            x2, y2 = y2, x2
 
-		# Filter line/track according to length and duration
-		if abs(run_duration) < minimal_duration:
-			continue
-		if abs(run_length) < minimal_length:
-			continue
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
 
-		# Save it to an IJ ResultsTable
-		rt.incrementCounter()
-		rt.addValue("Track ID", i+1)
-		rt.addValue("ROI ID", roi.name)
-		rt.addValue("Track  (um)", abs(run_length))
-		rt.addValue("Track duration (s)", abs(run_duration))
-		rt.addValue("Track speed (um/s)", run_speed)
-		rt.addValue("x1", x1)
-		rt.addValue("y1", y1)
-		rt.addValue("x2", x2)
-		rt.addValue("y2", y2)	
-	
-	rt.show('Comet Analysis Results for "%s"' % name)
+        # Compute line features
+        if not is_x_time:
+            run_length = (x1 - x2) * pixel_length
+            run_duration = (y2 - y1) * dt
+        else:
+            run_length = (y1 - y2) * pixel_length
+            run_duration = (x2 - x1) * dt
 
-	if save_results:
-		results_path = roi_path = os.path.splitext(fname)[0] + ".Results.csv"
-		rt.save(results_path)
+        run_speed = run_length / run_duration
 
-	if save_roi:
-		roi_path = os.path.splitext(fname)[0] + ".ROI.zip"
-		rm.runCommand("Save", roi_path);
+        # Filter line/track according to length and duration
+        if abs(run_duration) < minimal_duration:
+            continue
+        if abs(run_length) < minimal_length:
+            continue
+
+        # Save it to an IJ ResultsTable
+        rt.incrementCounter()
+        rt.addValue("Track ID", i+1)
+        rt.addValue("ROI ID", roi.name)
+        rt.addValue("Track  (um)", abs(run_length))
+        rt.addValue("Track duration (s)", abs(run_duration))
+        rt.addValue("Track speed (um/s)", run_speed)
+        rt.addValue("x1", x1)
+        rt.addValue("y1", y1)
+        rt.addValue("x2", x2)
+        rt.addValue("y2", y2)
+
+    rt.show('Comet Analysis Results for "%s"' % name)
+
+    if save_results:
+        results_path = roi_path = os.path.splitext(fname)[0] + ".Results.csv"
+        rt.save(results_path)
+
+    if save_roi:
+        roi_path = os.path.splitext(fname)[0] + ".ROI.zip"
+        rm.runCommand("Save", roi_path);
 
 main()
