d199570c1d6cae9796d23da9f8b2d63de79833e1
Hadrien Mary <hadrien.mary@gmail.com>
2019-02-15 13:18:10 -0500

    cro image when saving in a script

--- src/main/resources/script_templates/Hadim_Scripts/ROI/ROI_and_Image_saver.py
+++ src/main/resources/script_templates/Hadim_Scripts/ROI/ROI_and_Image_saver.py
@@ -1,10 +1,31 @@
-# @Boolean(label="Reset ROI Manager") reset_roi
 # @File(label="Select a directory", style="directory") parent_dir
 # @Dataset dataset
 # @DatasetIOService io
+# @OpService ops
+# @DatasetService ds
 
 import os
 from ij.plugin.frame import RoiManager
+
+def get_axis(axis_type):
+	from net.imagej.axis import Axes
+	return {'X': Axes.X, 'Y': Axes.Y, 'Z': Axes.Z, 'TIME': Axes.TIME,
+		     'CHANNEL': Axes.CHANNEL}.get(axis_type, Axes.Z)
+ 
+def crop(ops, data, intervals):
+	from net.imglib2.util import Intervals
+	
+	intervals_start = [data.min(d) for d in range(0, data.numDimensions())]
+	intervals_end = [data.max(d) for d in range(0, data.numDimensions())]
+ 
+	for axis_type, interval in intervals.items():
+		index = data.dimensionIndex(get_axis(axis_type))
+		intervals_start[index] = interval[0]
+		intervals_end[index] = interval[1]
+ 
+	intervals = Intervals.createMinMax(*intervals_start + intervals_end)
+	output = ops.run("transform.crop", data, intervals, True)
+	return output
 
 rm = RoiManager.getInstance()
 filename = dataset.getName()
@@ -16,5 +37,12 @@ rm.runCommand("Deselect")
 rm.runCommand("Save", roi_path)
 print("ROIs saved at {}".format(roi_path))
 
-io.save(dataset, image_path)
+# Crop the image
+z_position = rm.getRoi(0).getZPosition()
+intervals = {'Z': [z_position, z_position]}
+output = crop(ops, dataset, intervals)
+output = ops.run("transform.dropSingletonDimensionsView", output)
+output = ds.create(output)
+
+io.save(output, image_path)
 print("Image saved at {}".format(image_path))
