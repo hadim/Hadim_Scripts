1f9932a52ddb8216d1b85e5b2a3fa542167b493c
Hadrien Mary <hadrien.mary@gmail.com>
2019-02-20 16:00:07 -0500

    fix ROI_and_Image_saver.py

--- src/main/resources/script_templates/Hadim_Scripts/ROI/ROI_and_Image_saver.py
+++ src/main/resources/script_templates/Hadim_Scripts/ROI/ROI_and_Image_saver.py
@@ -11,18 +11,18 @@ def get_axis(axis_type):
 	from net.imagej.axis import Axes
 	return {'X': Axes.X, 'Y': Axes.Y, 'Z': Axes.Z, 'TIME': Axes.TIME,
 		     'CHANNEL': Axes.CHANNEL}.get(axis_type, Axes.Z)
- 
+
 def crop(ops, data, intervals):
 	from net.imglib2.util import Intervals
-	
+
 	intervals_start = [data.min(d) for d in range(0, data.numDimensions())]
 	intervals_end = [data.max(d) for d in range(0, data.numDimensions())]
- 
+
 	for axis_type, interval in intervals.items():
 		index = data.dimensionIndex(get_axis(axis_type))
 		intervals_start[index] = interval[0]
 		intervals_end[index] = interval[1]
- 
+
 	intervals = Intervals.createMinMax(*intervals_start + intervals_end)
 	output = ops.run("transform.crop", data, intervals, True)
 	return output
@@ -30,8 +30,8 @@ def crop(ops, data, intervals):
 rm = RoiManager.getInstance()
 filename = dataset.getName()
 
-roi_path = os.path.join(parent_dir.toString(), os.path.splitext(filename)[0] + ".zip")
-image_path = os.path.join(parent_dir.toString(), os.path.splitext(filename)[0] + ".tif")
+roi_path = os.path.join(parent_dir.toString(), filename.replace(".lif", "") + ".zip")
+image_path = os.path.join(parent_dir.toString(), filename.replace(".lif", "") + ".tif")
 
 rm.runCommand("Deselect")
 rm.runCommand("Save", roi_path)
